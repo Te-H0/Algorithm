03c4b32501452ddc99dbeece044a70e88cb821f6
xogh8755 <xogh8755@naver.com>
2023-01-03 15:18:47 +0900

    23.01.02

--- programmers/42577.py
+++ programmers/42577.py
@@ -1,15 +1,13 @@
 def solution(phone_book):
     answer = True
+    phone_book.sort()
+    
     for i in range(len(phone_book)-1):
-        for j in range(i+1, len(phone_book)):
-            if(len(phone_book[i]) <= len(phone_book[j])):
-                if(phone_book[i] == phone_book[j][:len(phone_book[i])]):
-                    answer = False
-                    break
-        if(not answer):
-            break
+        if phone_book[i+1].startswith(phone_book[i]):
+            answer=False;
+            break;
     return answer
 
 
-phone_book = ["4", "1111", "1111234"]
+phone_book = ["123","456","789"]
 print(solution(phone_book))
