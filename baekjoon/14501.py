f426e4dd4741c2245bac686cddf7e7a0b3f3a16e
xogh8755 <xogh8755@naver.com>
2023-07-01 18:48:49 +0900

    23-07-01

--- baekjoon/14501.py
+++ baekjoon/14501.py
@@ -11,18 +11,17 @@ for i in range(n):
     t.append(a)
     p.append(b)
 
-dp = copy.deepcopy(p)
+dp=[0]*(n+1)
+if t[1]<=n:
+    dp[1]=p[1]
 
-for i in range(1, n+1):
-    for j in range(1, i):
-
-        if (j+t[j] <= i):
-            if i == 4:
-                print('i>>>', i, 'j>>>>', j, 'pi>>>>', p[i], 'dp[j]+p[i]>>>', dp[j]+p[i])
-            dp[i] = max(p[i], dp[j]+p[i])
-        else:
-            dp[i] = p[i]
-    if i+t[i] > n+1:
-        dp[i] = max(dp[:i])
-
-print(dp)
+for i in range(2,n+1):
+    for j in range(1,i):
+        if j+t[j]<=i:
+            if i+t[i]<=n+1:
+                dp[i]=max(dp[j]+p[i],dp[i])
+        
+    if (dp[i]==0) and (i+t[i]<=n+1):
+        dp[i]=p[i]
+        
+print(max(dp))
