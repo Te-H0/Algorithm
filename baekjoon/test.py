b63afcc9002942071a21a9046a8e840104747b70
xogh8755 <xogh8755@naver.com>
2023-09-13 23:27:25 +0900

    .

--- baekjoon/test.py
+++ baekjoon/test.py
@@ -3,6 +3,18 @@ from re import L
 import sys
 from collections import deque, defaultdict
 from bisect import bisect
+import itertools
 
-l = [1, 2, 3]
-print(l.pop())
+# # 세 개의 리스트를 정의합니다.
+# list1 = [1, 2, 3]
+# list2 = [4, 5, 6]
+# list3 = [7, 8, 9]
+
+# # 세 개의 리스트에서 각각 하나의 요소를 선택하는 모든 조합을 생성합니다.
+# combinations = list(itertools.product(list1, list2, list3))
+
+# # 모든 조합을 출력합니다.
+# for combo in combinations:
+#     print((list(combo)))
+a = '1'
+print(int(a))
