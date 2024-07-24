cce0f1d77764be89b30bb6e85007e33ee4ad31f6
teho <xogh8755@naver.com>
2024-07-24 10:23:34 +0900

    24-07-24

--- baekjoon/4179.py
+++ baekjoon/4179.py
@@ -1,61 +1,72 @@
-from fileinput import nextfile
 import sys
 from collections import deque
-move = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
+move = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+WALL = -2
+jihon_start = (0, 0)
 r, c = map(int, sys.stdin.readline().split())
-maze = [(list(sys.stdin.readline().rstrip())) for _ in range(r)]
-s_i = 0  # jihoon start i
-s_j = 0  # jihoon start j
-fire_idx = []
+graph = [list(map(str, sys.stdin.readline().strip())) for _ in range(r)]
+# 양수는 불이 생긴 시간, -2는 벽, -1은 지훈이가 다녀간적이 있는 곳
+barrier = [[0] * c for _ in range(r)]
+first_fire = []
+
+
+def fire(first_fire): # 벽이 아닌 곳에 불이 퍼지는 시간 기록
+    dq = deque()
+    for x, y in first_fire:
+        if not barrier[x][y]:
+            barrier[x][y] = 1
+            dq.append((x, y, 1))
+    while dq:
+        x, y, t = dq.popleft()
+
+        for m_x, m_y in move:
+            n_x = x + m_x
+            n_y = y + m_y
+            if (
+                n_x in range(r)
+                and n_y in range(c)
+                and not barrier[n_x][n_y]
+                and barrier[n_x][n_y] != WALL
+            ):
+                barrier[n_x][n_y] = t + 1
+                dq.append((n_x, n_y, t + 1))
+
+
+def escape(i, j):
+    if i == r - 1 or j == c - 1 or i==0 or j==0:
+        return 1 # 시작 지점이 탈출 지점인 경우
+    dq = deque()
+    barrier[i][j] = -1
+    dq.append((i, j, 1))
+    while dq:
+        x, y, t = dq.popleft()
+        for m_x, m_y in move:
+            n_x = x + m_x
+            n_y = y + m_y
+            if ( # 다녀간적 없고 -1, 벽 아니고 WALL, 불이 퍼지기 전이면
+                n_x in range(r)
+                and n_y in range(c)
+                and barrier[n_x][n_y] != -1
+                and barrier[n_x][n_y] != WALL
+                and (barrier[n_x][n_y] > (t + 1) or not barrier[n_x][n_y])
+            ):
+                if n_x == r - 1 or n_y == c - 1 or n_x ==0 or n_y ==0:
+                    return t + 1
+                barrier[n_x][n_y] = -1
+                dq.append((n_x, n_y, t + 1))
+    return "IMPOSSIBLE"
+
+
 for i in range(r):
     for j in range(c):
-        if maze[i][j] == 'J':
-            s_i = i
-            s_j = j
-        elif maze[i][j] == 'F':
-            maze[i][j] = 1
-            fire_idx.append((i, j))
-        elif maze[i][j] == '.':
-            maze[i][j] = 0
+        if barrier[i][j] == 0:
+            if graph[i][j] == "F":
+                first_fire.append((i, j))
+            elif graph[i][j] == "#":
+                barrier[i][j] = WALL
+            elif graph[i][j] == "J":
+                jihon_start = (i, j)
 
-
-def fire():
-    dq = deque()
-    for i, j in fire_idx:
-        dq.append((i, j, 1))
-        maze[i][j] = 1
-    while dq:
-        i, j, time = dq.popleft()
-
-        for m_i, m_j in move:
-            n_i, n_j = i+m_i, j+m_j
-
-            if n_i in range(r) and n_j in range(c) and maze[n_i][n_j] == 0:
-                maze[n_i][n_j] = time + 1
-                dq.append((n_i, n_j, time+1))
-
-
-def bfs():
-    dq = deque()
-    dq.append((s_i, s_j, 1))
-    maze[s_i][s_j] = -1
-    while dq:
-        i, j, time = dq.popleft()
-        for m_i, m_j in move:
-            n_i, n_j = i+m_i, j+m_j
-            if n_i in range(r) and n_j in range(c):
-                if maze[n_i][n_j] != -1 and maze[n_i][n_j] != '#' and (maze[n_i][n_j] == 0 or maze[n_i][n_j] > time + 1):
-                    dq.append((n_i, n_j, time + 1))
-                    maze[n_i][n_j] = -1
-            else:
-                return time
-    return -1
-
-
-fire()
-result = bfs()
-if result == -1:
-    print("IMPOSSIBLE")
-else:
-    print(result)
+fire(first_fire)
+print(escape(jihon_start[0], jihon_start[1]))
