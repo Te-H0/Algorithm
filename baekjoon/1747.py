ee9fe7702ed0c5b5a0489575d18949dbe8a902ab
teho <xogh8755@naver.com>
2024-07-30 09:57:56 +0900

    24-07-30

--- baekjoon/1747.py
+++ baekjoon/1747.py
@@ -1,43 +1,26 @@
-from cmath import sqrt
-from math import ceil, floor
-import sys
-sys.setrecursionlimit(1000000)
-f = sys.stdin.readline()
+import sys,math
 
-n = int(f)
-result = False
+n = int(sys.stdin.readline())
+total = math.floor(math.sqrt(1500000))
+prime = [True] * 1500000
+prime[1] = False
 
+for i in range(2,total):
+    if prime[i]:
+        j=2
+        while i*j < 1500000:
+            prime[i*j] = False
+            j+=1
 
+def is_palindrome(x):
+    str_x = str(x)
+    size=len(str_x)
+    if size == 1:
+        return True    
+    return str_x[:size//2] == str_x[-1:-(size//2+1):-1]
 
-def palindrome(n):
-    result = False
-    while not result:
-        a= str(n)
-        half = len(a)//2
-        m = len(a)%2
-
-        if m==0:
-            result = a[:half] == a[len(a)-1:half-1:-1]
-        else:
-            result =  a[:half] == a[len(a)-1:half:-1]
-        
-        if result:
-           result = prime_number(n) 
-        
-        if not result:
-            n+=1
-    return n
-
-    
-
-def prime_number(n):
-    if n==1:
-        return False
-    for i in range(2, n//2+1):
-        if n % i == 0:
-            return False
-    return True
-
-
-
-print(palindrome(n))
+for i in range(n,1500000):
+    if prime[i]:
+        if is_palindrome(i):
+            print(i)
+            break
