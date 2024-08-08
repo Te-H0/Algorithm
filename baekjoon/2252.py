eb95d910e264475082d79de43e8755ef3d862838
teho <xogh8755@naver.com>
2024-08-08 10:23:22 +0900

    24-08-08

--- baekjoon/2252.py
+++ baekjoon/2252.py
@@ -1,39 +1,30 @@
 import sys
 from collections import deque
-f = sys.stdin.readline
 
-n, m = map(int, f().split())
-
-graph = [[]for i in range(n+1)]
-indegree = [0]*(n+1)
+n, m = map(int, sys.stdin.readline().split())
+li = [[] for _ in range(n + 1)]  # 내가 앞서 나와야 하는 수들
+indegree = [0] * (n + 1)
 answer = []
 
-for i in range(m):
-    a, b = map(int, f().split())
-    graph[a].append(b)
-    indegree[b] += 1
+for _ in range(m):
+    x, y = map(int, sys.stdin.readline().split())
+    li[x].append(y)
+    indegree[y] += 1
 
+dq = deque()
+for i in range(1, n + 1):
+    if not indegree[i]:
+        dq.append(i)
+        answer.append(i)
 
-def topology_sort():
-    dq = deque()
+while dq:
+    x = dq.popleft()
 
-    for i in range(1, n+1):
-        if indegree[i] == 0:
-            dq.append(i)
-            answer.append(i)
+    for l in li[x]:
+        indegree[l] -= 1
 
-    while dq:
-        x = dq.popleft()
+        if not indegree[l]:
+            dq.append(l)
+            answer.append(l)
 
-        for i in graph[x]:
-            indegree[i] -= 1
-
-            if indegree[i] == 0:
-                dq.append(i)
-                answer.append(i)
-
-
-topology_sort()
-
-for i in answer:
-    print(i, end=' ')
+print(*answer)
