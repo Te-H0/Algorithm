4b98293ba416626f399f4593e22ab48309624104
teho <xogh8755@naver.com>
2024-09-16 20:02:55 +0900

    24-09-16

--- baekjoon/1149.py
+++ baekjoon/1149.py
@@ -1,15 +1,18 @@
 import sys
 
 n = int(sys.stdin.readline())
-l = list()
-
-for i in range(n):
-    l.append(list(map(int, input().split())))
+cost=[list(map(int,sys.stdin.readline().split())) for _ in range(n)] # R, G, B 순서
+dp = [[0,0,0] for _ in range(n)] # R, G, B 순서 - i번째에서 각각 R, G, B를 선택한 값
+dp[0] = cost[0][:]
 
 for i in range(1,n):
-    l[i][0] = min(l[i-1][1],l[i-1][2])+ l[i][0]
-    l[i][1] = min(l[i-1][0],l[i-1][2])+ l[i][1]
-    l[i][2] = min(l[i-1][0],l[i-1][1])+ l[i][2]
-
-
-print(min(l[n-1]))
+    #R을 선택한 경우
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2] )+ cost[i][0]
+    
+    #G를 선택한 경우
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2])+ cost[i][1]
+    
+    #B를 선택한 경우
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1] ) + cost[i][2]
+    
+print(min(dp[n-1]))
